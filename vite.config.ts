bc771e55553b5ae5ba09e8067cf516aca30a69c9
santiagogustavo <santiago.gustavo.mail@gmail.com>
2023-05-10 23:55:52 -0300

    ci(deploy): add deploy script, add push-dir

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import vue from '@vitejs/plugin-vue';
 const path = require('path');
 
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    ssr: false,
   },
 });
